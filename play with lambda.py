dabf8dddaf1e1d489772a18416f965c3138e5416
Shanmukha-Mohnish <116248130+Shanmukha-Mohnish@users.noreply.github.com>
2023-07-06 21:22:03 +0530

    Add files via upload

---
+++ play with lambda.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+r=lambda a:a+25
+
+a=int (input("enter the value: "))
+print(r(a))
+
+
+# In[ ]:
+
+
+
+
---
+++ triple all numbers in a list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+lst=[1,2,3,4,5,6,7,8,9]
+print("given list:",lst)
+result=map(lambda x: x+x+x,lst)
+print("Triple of given list :")
+print(list(result))
+
+
+# In[ ]:
+
+
+
+
